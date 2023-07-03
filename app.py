6ba693e6ac074a9ae415c3605f91a5a84dc0b24c
divyavinod6 <thisisdivyavinod@gmail.com>
2023-07-03 23:58:19 +0530

    updated index

--- app.py
+++ app.py
@@ -9,7 +9,32 @@ def index():
     car_models=sorted(df['Model'].unique())
     years=sorted(df['Year'].unique(),reverse=True)
     fuel_type=df['Fuel_Type'].unique()
-    return render_template('index.html',companies=companies,car_models=car_models,years=years,fuel_type=fuel_type)
+    locations = sorted(df['Location'].unique())
+    kilometers_driven = sorted(df['Kilometers_Driven'].unique())
+    fuel_types = df['Fuel_Type'].unique()
+    transmissions = df['Transmission'].unique()
+    owner_types = df['Owner_Type'].unique()
+    mileages = sorted(df['Mileage'].unique())
+    engines = sorted(df['Engine'].unique())
+    powers = sorted(df['Power'].unique())
+    seats = sorted(df['Seats'].unique())
+
+    options = {
+        'companies':companies,
+        'car_models':car_models,
+        'years':years,
+        'fuel_type':fuel_type,
+        'locations': locations,
+        'years': years,
+        'kilometers_driven': kilometers_driven,
+        'transmissions': transmissions,
+        'owner_types': owner_types,
+        'mileages': mileages,
+        'engines': engines,
+        'powers': powers,
+        'seats': seats
+    }
+    return render_template('index.html',options=options)
 
 if __name__=='__main__':
     app.run(debug=True)
