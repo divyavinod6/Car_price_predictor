80a00b37922fddbef029357ffccf3ca3db3c1a3c
divyavinod6 <thisisdivyavinod@gmail.com>
2023-07-04 20:32:35 +0530

    completed XMLHTTP request

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import *
 import pandas as pd
+import numpy as np
+import pickle
 app=Flask(__name__)
 
+model=pickle.load(open("car_price_prediction_model.pkl","rb"))
 df=pd.read_csv('cleaned-data.csv')
 @app.route('/')
 def index():
@@ -25,7 +28,6 @@ def index():
         'years':years,
         'fuel_type':fuel_type,
         'locations': locations,
-        'years': years,
         'kilometers_driven': kilometers_driven,
         'transmissions': transmissions,
         'owner_types': owner_types,
@@ -34,7 +36,32 @@ def index():
         'powers': powers,
         'seats': seats
     }
+    # Convert int64 values to Python integers
+    # options = {k: v.tolist() if isinstance(v, np.ndarray) and v.dtype == 'int64' else v for k, v in options.items()}
+
+    # options_json = json.dumps(options)  # Convert options dictionary to JSON-encoded string
+
     return render_template('index.html',options=options)
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    company= request.form.get('company')
+    car_model=request.form.get('car_model')
+    years=int(request.form.get('years'))
+    fuel_type=request.form.get('fuel_type')
+    location=request.form.get('location')
+    kilometers_driven=int(request.form.get('kilometers_driven'))
+    transmissions=request.form.get('transmissions')
+    owner_types=int(request.form.get('owner_types'))
+    mileages=float(request.form.get('mileages'))
+    engines=float(request.form.get('engines'))
+    powers=float(request.form.get('powers'))
+    seats=float(request.form.get('seats'))
+    # print(company,car_model,years,fuel_type,kilometers_driven,transmissions,owner_types,mileages,engines,powers,seats)
+    
+    prediction = model.predict(pd.DataFrame([[car_model, company, years, kilometers_driven, transmissions,fuel_type,location, owner_types, mileages, engines, powers, seats]], 
+                            columns=['Model', 'Company', 'Year', 'Kilometers_Driven', 'Transmission','Fuel_Type','Location','Owner_Type', 'Mileage', 'Engine', 'Power', 'Seats']))
+    
+    return str(np.round(prediction[0],2))
 if __name__=='__main__':
     app.run(debug=True)
